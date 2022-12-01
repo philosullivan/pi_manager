8a7faee7c7ad57b9b18416e6dfd96400606b2d7c
PhilOSullivan <phil.osullivan@gmail.com>
2022-12-01 07:26:35 -0500

    window state

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {
 }                       = require( 'electron' );
 const path              = require( 'path' );
 const os                = require( 'os' );
+const windowStateKeeper = require( 'electron-window-state' );
 const user_home_dir     = os.homedir();
 const app_path          = app.getAppPath();
 const app_name          = app.getName();
@@ -20,9 +21,19 @@ if ( is_dev ) {
 
 // .
 const createWindow = () => {
+
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  });
+
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    x : mainWindowState.x,
+    y : mainWindowState.y,
+    width : mainWindowState.width,
+    height : mainWindowState.height,
+    minWidth:500,
+    minHeight:628,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -33,11 +44,16 @@ const createWindow = () => {
     },
   });
 
+  // .
+  mainWindowState.manage( mainWindow );
+
   // and load the index.html of the app.
   mainWindow.loadFile( `${template_index}` );
 
+  mainWindow.setMenuBarVisibility(false);
+
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // .
@@ -45,9 +61,7 @@ app.on('ready', createWindow);
 
 // .
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
     app.quit();
-  }
 });
 
 // .
